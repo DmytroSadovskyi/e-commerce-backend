f870ed4d77d44286df7440e002a60d79a95e8b0f
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2024-09-04 16:51:49 +0300

    add base structure

---
+++ helpers/index.js
@@ -0,0 +1,8 @@
+const ctrlWrapper = require("./ctrlWrapper");
+const HttpError = require("./httpError");
+const handleMongooseError = require("./handleMongooseError");
+module.exports = {
+  ctrlWrapper,
+  HttpError,
+  handleMongooseError,
+};
---
+++ models/product.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require("mongoose");
+const { httpError } = require("../helpers");
+
+const productSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  discountPercentage: {
+    type: Number,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  stock: {
+    type: Number,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+  },
+  images: {
+    type: String,
+    required: true,
+  },
+});
+
+productSchema.post("save", httpError);
+
+const Product = mongoose.model("product", productSchema);
+
+exports.Product = Product;
